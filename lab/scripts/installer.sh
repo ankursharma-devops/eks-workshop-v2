e21b7d610b1b8e2b6a600bfa3a10ef70ad9bd7e6
ankursharma-devops <ankur.sharma@infracloud.io>
2024-02-20 18:40:42 +0530

    Fix issues in setup.sh

--- lab/scripts/installer.sh
+++ lab/scripts/installer.sh
@@ -117,55 +117,55 @@ set -e
 curl -fsSL https://raw.githubusercontent.com/${REPOSITORY_OWNER}/${REPOSITORY_NAME}/$REPOSITORY_REF/lab/bin/reset-environment | bash -s -- \$1
 EOT"
   sudo chmod +x /usr/local/bin/reset-environment
-  sudo bash -c 'cat << EOT > /usr/local/bin/delete-environment
+  sudo -E bash -c 'cat << EOT > /usr/local/bin/delete-environment
 #!/bin/bash
 set -e
 curl -fsSL https://raw.githubusercontent.com/${REPOSITORY_OWNER}/${REPOSITORY_NAME}/$REPOSITORY_REF/lab/bin/delete-environment | bash
 EOT'
   sudo chmod +x /usr/local/bin/delete-environment
-  sudo bash -c 'cat << EOT > /usr/local/bin/wait-for-lb
+  sudo -E bash -c 'cat << EOT > /usr/local/bin/wait-for-lb
 #!/bin/bash
 set -e
 curl -fsSL https://raw.githubusercontent.com/${REPOSITORY_OWNER}/${REPOSITORY_NAME}/$REPOSITORY_REF/lab/bin/wait-for-lb | bash -s -- \$1
 EOT'
   sudo chmod +x /usr/local/bin/wait-for-lb
-  sudo bash -c 'cat << EOT > /usr/local/bin/use-cluster
+  sudo -E bash -c 'cat << EOT > /usr/local/bin/use-cluster
 #!/bin/bash
 set -e
 curl -fsSL https://raw.githubusercontent.com/${REPOSITORY_OWNER}/${REPOSITORY_NAME}/$REPOSITORY_REF/lab/bin/use-cluster | bash -s -- \$1
 EOT'
   sudo chmod +x /usr/local/bin/use-cluster
-  sudo bash -c 'cat << EOT > /usr/local/bin/awshttp
+  sudo -E bash -c 'cat << EOT > /usr/local/bin/awshttp
 #!/bin/bash
 set -e
 curl -fsSL https://raw.githubusercontent.com/${REPOSITORY_OWNER}/${REPOSITORY_NAME}/$REPOSITORY_REF/lab/bin/awshttp | bash -s -- \$1
 EOT'
   sudo chmod +x /usr/local/bin/awshttp
-  sudo bash -c 'cat << EOT > /usr/local/bin/delete-all-if-crd-exists
+  sudo -E bash -c 'cat << EOT > /usr/local/bin/delete-all-if-crd-exists
 #!/bin/bash
 set -e
 curl -fsSL https://raw.githubusercontent.com/${REPOSITORY_OWNER}/${REPOSITORY_NAME}/$REPOSITORY_REF/lab/bin/delete-all-if-crd-exists | bash -s -- \$@
 EOT'
   sudo chmod +x /usr/local/bin/delete-all-if-crd-exists
-  sudo bash -c 'cat << EOT > /usr/local/bin/delete-all-and-wait-if-crd-exists
+  sudo -E bash -c 'cat << EOT > /usr/local/bin/delete-all-and-wait-if-crd-exists
 #!/bin/bash
 set -e
 curl -fsSL https://raw.githubusercontent.com/${REPOSITORY_OWNER}/${REPOSITORY_NAME}/$REPOSITORY_REF/lab/bin/delete-all-and-wait-if-crd-exists | bash -s -- \$@
 EOT'
   sudo chmod +x /usr/local/bin/delete-all-and-wait-if-crd-exists
-  sudo bash -c 'cat << EOT > /usr/local/bin/delete-nodegroup
+  sudo -E bash -c 'cat << EOT > /usr/local/bin/delete-nodegroup
 #!/bin/bash
 set -e
 curl -fsSL https://raw.githubusercontent.com/${REPOSITORY_OWNER}/${REPOSITORY_NAME}/$REPOSITORY_REF/lab/bin/delete-nodegroup | bash -s -- \$1
 EOT'
   sudo chmod +x /usr/local/bin/delete-nodegroup
-  sudo bash -c 'cat << EOT > /usr/local/bin/uninstall-helm-chart
+  sudo -E bash -c 'cat << EOT > /usr/local/bin/uninstall-helm-chart
 #!/bin/bash
 set -e
 curl -fsSL https://raw.githubusercontent.com/${REPOSITORY_OWNER}/${REPOSITORY_NAME}/$REPOSITORY_REF/lab/bin/uninstall-helm-chart | bash -s -- \$@
 EOT'
   sudo chmod +x /usr/local/bin/uninstall-helm-chart
-  sudo bash -c 'cat << EOT > /usr/local/bin/update-ide
+  sudo -E bash -c 'cat << EOT > /usr/local/bin/update-ide
 #!/bin/bash
 set -e
 curl -fsSL https://raw.githubusercontent.com/${REPOSITORY_OWNER}/${REPOSITORY_NAME}/$REPOSITORY_REF/lab/bin/update-ide | bash
