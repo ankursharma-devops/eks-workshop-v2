979a3fc1a85d1fd45d6e9a41b86fa7830076ae06
ankursharma-devops <ankur.sharma@infracloud.io>
2024-03-08 00:34:14 +0530

    Added rds cleanup

--- blackbeard-setup/install_app.sh
+++ blackbeard-setup/install_app.sh
@@ -29,7 +29,7 @@ ingress () {
   kubectl get ingress -n ui ui -o jsonpath="{.status.loadBalancer.ingress[*].hostname}{'\n'}"
   echo -e "\n\n##############################################################################\n\n"
   # wait for AWS loadBalancer to become ready
-#  wait-for-lb $(kubectl get ingress -n ui ui -o jsonpath="{.status.loadBalancer.ingress[*].hostname}{'\n'}")
+  wait-for-lb $(kubectl get ingress -n ui ui -o jsonpath="{.status.loadBalancer.ingress[*].hostname}{'\n'}")
   echo -e "\n\n####################\nIngress setup completed\n\n#######################"
 }
 
@@ -143,33 +143,60 @@ ack_dynamodb () {
 }
 
 ack_rds () {
-  echo -e "\n\n####################\nSetup RDS MYSQL instance using ACK operator\n\n#######################"
-  # generate required variables
-  RDS_NAMESPACE=rds-ack
-  RDS_SUBNET_GROUP_NAME="ack-rds-subnet-group"
-  RDS_SUBNET_GROUP_DESCRIPTION="RDS subnet group"
-  EKS_VPC_ID=$(aws eks describe-cluster --name="${EKS_CLUSTER_NAME}" --region $AWS_REGION \
-   --query "cluster.resourcesVpcConfig.vpcId" \
-   --output text)
-  EKS_SUBNET_IDS=$(aws ec2 describe-subnets --region $AWS_REGION \
-    --filters "Name=vpc-id,Values=${EKS_VPC_ID}" \
-    --query 'Subnets[*].SubnetId' \
-    --output text)
-  EKS_CIDR_RANGE=$(aws ec2 describe-vpcs \
-   --vpc-ids $EKS_VPC_ID \
-   --query "Vpcs[].CidrBlock" \
-   --output text)
-  # create namespace if not exist
-  kubectl create namespace $RDS_NAMESPACE --dry-run=client -o yaml | kubectl apply -f-
-  # Use irsa and attach role to service account in k8s
-  eksctl create iamserviceaccount --name ack-rds-controller --region $AWS_REGION \
-    --namespace $RDS_NAMESPACE --cluster $EKS_CLUSTER_NAME \
-    --role-name ${EKS_CLUSTER_NAME}-ack-rds-controller \
-    --attach-policy-arn arn:aws:iam::aws:policy/AmazonRDSFullAccess --approve
-  aws ecr-public get-login-password --region $AWS_REGION | helm registry login --username AWS --password-stdin public.ecr.aws
-  # Install ack-rds-controller using helm
-  helm install -n $RDS_NAMESPACE ack-rds oci://public.ecr.aws/aws-controllers-k8s/rds-chart --version=0.0.27 --set=aws.region=$AWS_REGION --set serviceAccount.create=false
-  # create manifest for rds subnet group
+  if [ "$DESTROY" == "true" ]
+  then
+    # vars
+    RDS_SUBNET_GROUP_NAME="ack-rds-subnet-group"
+    RDS_NAMESPACE=rds-ack
+    ## delete rds instance catalog db
+    #
+    RDS_INSTANCE_NAME="catalog-ack-mysql"
+    APP_NAMESPACE="catalog"
+    kubectl delete dbinstance $RDS_INSTANCE_NAME -n $APP_NAMESPACE
+    ## delete rds instance orders db
+    #
+    RDS_INSTANCE_NAME="orders-ack-mysql"
+    APP_NAMESPACE="orders"
+    kubectl delete dbinstance $RDS_INSTANCE_NAME -n $APP_NAMESPACE
+    ## delete rds subnet group
+    kubectl delete dbsubnetgroup ${RDS_SUBNET_GROUP_NAME} -n $RDS_NAMESPACE
+    ## delete security group
+    aws ec2 delete-security-group --region $AWS_REGION  \
+      --group-name "${RDS_SUBNET_GROUP_NAME}"
+    ## delete rds-ack-controller
+    helm delete -n $RDS_NAMESPACE ack-rds
+    # delete irsa 
+    eksctl delete iamserviceaccount --name ack-rds-controller --region $AWS_REGION \
+      --cluster $EKS_CLUSTER_NAME
+
+  else
+    echo -e "\n\n####################\nSetup RDS MYSQL instance using ACK operator\n\n#######################"
+    # generate required variables
+    RDS_NAMESPACE=rds-ack
+    RDS_SUBNET_GROUP_NAME="ack-rds-subnet-group"
+    RDS_SUBNET_GROUP_DESCRIPTION="RDS subnet group"
+    EKS_VPC_ID=$(aws eks describe-cluster --name="${EKS_CLUSTER_NAME}" --region $AWS_REGION \
+     --query "cluster.resourcesVpcConfig.vpcId" \
+     --output text)
+    EKS_SUBNET_IDS=$(aws ec2 describe-subnets --region $AWS_REGION \
+      --filters "Name=vpc-id,Values=${EKS_VPC_ID}" \
+      --query 'Subnets[*].SubnetId' \
+      --output text)
+    EKS_CIDR_RANGE=$(aws ec2 describe-vpcs \
+     --vpc-ids $EKS_VPC_ID \
+     --query "Vpcs[].CidrBlock" \
+     --output text)
+    # create namespace if not exist
+    kubectl create namespace $RDS_NAMESPACE --dry-run=client -o yaml | kubectl apply -f-
+    # Use irsa and attach role to service account in k8s
+    eksctl create iamserviceaccount --name ack-rds-controller --region $AWS_REGION \
+      --namespace $RDS_NAMESPACE --cluster $EKS_CLUSTER_NAME \
+      --role-name ${EKS_CLUSTER_NAME}-ack-rds-controller \
+      --attach-policy-arn arn:aws:iam::aws:policy/AmazonRDSFullAccess --approve
+    aws ecr-public get-login-password  | helm registry login --username AWS --password-stdin public.ecr.aws # --region $AWS_REGION
+    # Install ack-rds-controller using helm
+    helm upgrade --install -n $RDS_NAMESPACE ack-rds oci://public.ecr.aws/aws-controllers-k8s/rds-chart --version=0.0.27 --set=aws.region=$AWS_REGION --set serviceAccount.create=false
+    # create manifest for rds subnet group
 cat <<-EOF > db-subnet-groups.yaml
 apiVersion: rds.services.k8s.aws/v1alpha1
 kind: DBSubnetGroup
@@ -182,96 +209,102 @@ spec:
   subnetIDs:
 $(printf "    - %s\n" ${EKS_SUBNET_IDS})
 EOF
-  # apply manifest
-  kubectl apply -f db-subnet-groups.yaml
-  # Wait for subnet grup to complete
-  for i in $(seq 6)
-  do
-    STATUS=$(kubectl get dbsubnetgroup $RDS_SUBNET_GROUP_NAME -ojson -n ${RDS_NAMESPACE} | jq -r '.status.subnetGroupStatus')
-    if [ $STATUS == "Complete" ]
-    then
-      break
-    else
-      echo "waiting for rds subnet group creation to complete"
-      sleep 10s
-    fi
-  done
-  # create security group and add inbound traffic rule
-  RDS_SECURITY_GROUP_ID=$(aws ec2 create-security-group --region $AWS_REGION  \
-    --group-name "${RDS_SUBNET_GROUP_NAME}" \
-    --description "${RDS_SUBNET_GROUP_DESCRIPTION}" \
-    --vpc-id "${EKS_VPC_ID}" \
-    --output text)
-  aws ec2 authorize-security-group-ingress --region $AWS_REGION \
-     --group-id "${RDS_SECURITY_GROUP_ID}" \
-     --protocol tcp \
-     --port 3306 \
-     --cidr "${EKS_CIDR_RANGE}"
-  ## create rds db instance for catalog app
-  ##
-  echo -e "###  Creating RDS postgresql for catalog app \n"
-  RDS_INSTANCE_NAME="catalog-ack-mysql"
-  APP_NAMESPACE="catalog"
-  DB_ENGINE="mysql"
-  RDS_DB_NAME="catalog"
-  # Set password for DB
-  kubectl create secret generic -n $APP_NAMESPACE "${RDS_INSTANCE_NAME}-password" \
-  --from-literal=password='beard!#black' --from-literal=username=admin
-  # create instance
-  cat ~/environment/eks-workshop/modules/ack-rds/rds-mysql.yaml | envsubst  | k apply -f-
-  # wait for rds to create and generate endpoint
-  for i in $(seq 10)
-  do
-    STATUS=$(kubectl get dbinstance $RDS_INSTANCE_NAME  -n ${APP_NAMESPACE} -o json | jq -r '.status.dbInstanceStatus')
-    if [ $STATUS == "available" ] || [ $STATUS == "backing" ]
-    then
-      break
-    else
-      echo -e "rds not active retrying....\nwaiting for rds creation to complete"
-      sleep 30s
-    fi
-  done
-  #
-  RDS_DB_ENDPOINT=$(kubectl get dbinstance $RDS_INSTANCE_NAME -n ${APP_NAMESPACE} -o json | jq -r '.status.endpoint.address')
-  cat ~/environment/eks-workshop/modules/ack-rds/catalog-patch.yaml | envsubst  | k apply -f-
-  kubectl create secret generic -n $APP_NAMESPACE "catalog-db-ack" \
-    --from-literal=password='beard!#black' --from-literal=username=admin
-  # Update catalog app to use rds
-  kubectl kustomize ~/environment/eks-workshop/modules/ack-rds/catalog-kustomize | envsubst | kubectl apply -f-
-  echo -e "### RDS for catalog app created and application updated\n"
+    # apply manifest
+    kubectl apply -f db-subnet-groups.yaml
+    # Wait for subnet grup to complete
+    for i in $(seq 6)
+    do
+      STATUS=$(kubectl get dbsubnetgroup $RDS_SUBNET_GROUP_NAME -ojson -n ${RDS_NAMESPACE} | jq -r '.status.subnetGroupStatus')
+      if [ "$STATUS" == "Complete" ]
+      then
+        break
+      else
+        echo "waiting for rds subnet group creation to complete"
+        sleep 10s
+      fi
+    done
+    # create security group and add inbound traffic rule
+    RDS_SECURITY_GROUP_ID=$(aws ec2 create-security-group --region $AWS_REGION  \
+      --group-name "${RDS_SUBNET_GROUP_NAME}" \
+      --description "${RDS_SUBNET_GROUP_DESCRIPTION}" \
+      --vpc-id "${EKS_VPC_ID}" \
+      --output text)
+    aws ec2 authorize-security-group-ingress --region $AWS_REGION \
+       --group-id "${RDS_SECURITY_GROUP_ID}" \
+       --protocol tcp \
+       --port 3306 \
+       --cidr "${EKS_CIDR_RANGE}"
 
-  ## create rds db instance for orders app
-  ##
-  echo -e "###  Creating RDS postgresql for orders app \n"
-  RDS_INSTANCE_NAME="orders-ack-mysql"
-  APP_NAMESPACE="orders"
-  DB_ENGINE="mysql"
-  RDS_DB_NAME="orders"
-  # set password for db
-  kubectl create secret generic -n $APP_NAMESPACE "${RDS_INSTANCE_NAME}-password" \
-  --from-literal=password='beard!#black'
-  # create instance
-  cat ~/environment/eks-workshop/modules/ack-rds/rds-mysql.yaml | envsubst  | k apply -f-
-  # wait for rds to create and generate endpoint
-  for i in $(seq 10)
-  do
-    STATUS=$(kubectl get dbinstance $RDS_INSTANCE_NAME  -n ${APP_NAMESPACE} -o json | jq -r '.status.dbInstanceStatus')
-    if [ $STATUS == "available" ] || [ $STATUS == "backing" ]
-    then
-      break
-    else
-      echo -e "rds not active retrying....\nwaiting for rds creation to complete"
-      sleep 30s
-    fi
-  done
-  #
-  RDS_DB_ENDPOINT=$(kubectl get dbinstance $RDS_INSTANCE_NAME -n ${APP_NAMESPACE} -o json | jq -r '.status.endpoint.address')
-  ORDERS_DB_URL=$(echo "jdbc:mariadb://$RDS_DB_ENDPOINT:3306/$RDS_DB_NAME" | base64 )
-  cat ~/environment/eks-workshop/modules/ack-rds/orders-patch.yaml | envsubst  | k apply -f-
-  # Update catalog app to use rds
-  kubectl kustomize ~/environment/eks-workshop/modules/ack-rds/orders-kustomize | envsubst | kubectl apply -f-
-  echo -e "### RDS for orders app created and application updated\n"
 
+    ## create rds db instance for catalog app
+    ##
+
+    echo -e "###  Creating RDS postgresql for catalog app \n"
+    RDS_INSTANCE_NAME="catalog-ack-mysql"
+    APP_NAMESPACE="catalog"
+    DB_ENGINE="mysql"
+    export RDS_DB_NAME="catalog"
+    # Set password for DB
+    #
+    kubectl create secret generic -n $APP_NAMESPACE "${RDS_INSTANCE_NAME}-password" \
+    --from-literal=password='beard!#black' --from-literal=username=admin --dry-run=client -o yaml | kubectl apply -f-
+    # create instance
+    #
+    cat ~/environment/eks-workshop/modules/ack-rds/rds-mysql.yaml | envsubst  | kubectl apply -f-
+    # wait for rds to create and generate endpoint
+    #
+    for i in $(seq 10)
+    do
+      STATUS=$(kubectl get dbinstance $RDS_INSTANCE_NAME  -n ${APP_NAMESPACE} -o json | jq -r '.status.dbInstanceStatus')
+      if [ "$STATUS" == "available" ] || [ "$STATUS" == "backing-up" ]
+      then
+        break
+      else
+        echo -e "rds not active retrying....\nwaiting for rds creation to complete"
+        sleep 30s
+      fi
+    done
+    #
+    RDS_DB_ENDPOINT=$(kubectl get dbinstance $RDS_INSTANCE_NAME -n ${APP_NAMESPACE} -o json | jq -r '.status.endpoint.address')
+    cat ~/environment/eks-workshop/modules/ack-rds/catalog-patch.yaml | envsubst  | kubectl apply -f-
+    kubectl create secret generic -n $APP_NAMESPACE "catalog-db-ack" \
+      --from-literal=password='beard!#black' --from-literal=username=admin --dry-run=client -o yaml | kubectl apply -f-
+    # Update catalog app to use rds
+    kubectl kustomize ~/environment/eks-workshop/modules/ack-rds/catalog-kustomize | envsubst | kubectl apply -f-
+    echo -e "### RDS for catalog app created and application updated\n"
+
+    ## create rds db instance for orders app
+    ##
+    echo -e "###  Creating RDS postgresql for orders app \n"
+    RDS_INSTANCE_NAME="orders-ack-mysql"
+    APP_NAMESPACE="orders"
+    DB_ENGINE="mysql"
+    export RDS_DB_NAME="orders"
+    # set password for db
+    kubectl create secret generic -n $APP_NAMESPACE "${RDS_INSTANCE_NAME}-password" \
+    --from-literal=password='beard!#black'
+    # create instance
+    cat ~/environment/eks-workshop/modules/ack-rds/rds-mysql.yaml | envsubst  | kubectl apply -f-
+    # wait for rds to create and generate endpoint
+    for i in $(seq 10)
+    do
+      STATUS=$(kubectl get dbinstance $RDS_INSTANCE_NAME  -n ${APP_NAMESPACE} -o json | jq -r '.status.dbInstanceStatus')
+      if [ "$STATUS" == "available" ] || [ "$STATUS" == "backing-up" ]
+      then
+        break
+      else
+        echo -e "rds not active retrying....\nwaiting for rds creation to complete"
+        sleep 30s
+      fi
+    done
+    #
+    RDS_DB_ENDPOINT=$(echo "$(kubectl get dbinstance $RDS_INSTANCE_NAME -n ${APP_NAMESPACE} -o json | jq -r '.status.endpoint.address')")
+    export ORDERS_DB_URL=$(echo "jdbc:mariadb://$RDS_DB_ENDPOINT:3306/$RDS_DB_NAME" | base64 -w 0 )
+    cat ~/environment/eks-workshop/modules/ack-rds/orders-patch.yaml | envsubst  | kubectl apply -f-
+    # Update catalog app to use rds
+    kubectl kustomize ~/environment/eks-workshop/modules/ack-rds/orders-kustomize | envsubst | kubectl apply -f-
+    echo -e "### RDS for orders app created and application updated\n"
+  fi
 }
 
 
@@ -286,11 +319,11 @@ then
     echo "$@" >&1
   }
   export -f logmessage
-
+  # Remove ack RDS resources; manifests/module/ack-rds
+  ack_rds
   export TF_VAR_eks_cluster_id="$EKS_CLUSTER_NAME"
   #remove all installed modules
   for module in automation/controlplanes/ack observability/container-insights observability/oss-metrics observability/opensearch observability/logging/pods observability/logging/cluster exposing/ingress
-#  for module in observability/container-insights
   do
     echo -e "\n############################REMVOING LAB MODULE: $module \n############################\n"
     if [ -f "/eks-workshop/hooks/$module/cleanup.sh" ]; then
