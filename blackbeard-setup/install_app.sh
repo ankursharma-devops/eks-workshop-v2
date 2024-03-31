7e4b3ed63c1c27b12d10edfff897f01b338bd7cf
ankursharma-devops <ankur.sharma@infracloud.io>
2024-04-01 01:09:26 +0530

    set cloudwatch log retention to 3 days

--- blackbeard-setup/install_app.sh
+++ blackbeard-setup/install_app.sh
@@ -389,7 +389,8 @@ cloudwatch_logs_v1 () {
   --from-literal=read.tail=${FluentBitReadFromTail} \
   --from-literal=logs.region=${AWS_REGION} -n amazon-cloudwatch
   # deploy fluent bit daemonset
-  kubectl apply -f https://raw.githubusercontent.com/aws-samples/amazon-cloudwatch-container-insights/latest/k8s-deployment-manifest-templates/deployment-mode/daemonset/container-insights-monitoring/fluent-bit/fluent-bit.yaml
+  curl https://raw.githubusercontent.com/aws-samples/amazon-cloudwatch-container-insights/latest/k8s-deployment-manifest-templates/deployment-mode/daemonset/container-insights-monitoring/fluent-bit/fluent-bit.yaml \
+   | sed "s/cloudwatch_logs/cloudwatch_logs\\n\\tlog_retention_days\\t3/g" | kubectl apply -f-
 
   ## send container insights metrics to cloudwatch
   # Create a service account
